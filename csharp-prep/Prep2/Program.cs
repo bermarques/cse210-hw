8579db3d31c5b130bc032334b104aca814e467f9
Bernardo Marques <bernardohmarques@gmail.com>
2024-04-25 02:07:39 -0300

    create grading system

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+    Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+        string sign = "";
+        string letter;
+
+        if (percent % 10 >= 7) {
+            sign = "+";
+        }
+        else if (percent % 10 < 3) {
+            sign = "-";
+        }
+
+        if (percent >= 90)
+        {
+            letter = $"A{(sign == "+" ? "" : sign)}";
+        }
+        else if (percent >= 80)
+        {
+            letter = $"B{sign}";
+        }
+        else if (percent >= 70)
+        {
+            letter = $"C{sign}";
+        }
+        else if (percent >= 60)
+        {
+            letter = $"D{sign}";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
     }
 }
