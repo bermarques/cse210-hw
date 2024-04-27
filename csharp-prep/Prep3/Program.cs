d4b88a7c78a7a9abb66fc61e6438fe371700f7a3
Bernardo Marques <bernardohmarques@gmail.com>
2024-04-27 16:34:25 -0300

    create guess game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+
+        int answer;
+        int number = randomGenerator.Next(1, 100);
+        int guesses = 0;
+
+        do
+        {
+            Console.WriteLine("What is your guess?");
+            answer = int.Parse(Console.ReadLine());
+            guesses += 1;
+
+            if (number > answer)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < answer)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you{guesses} guesses!");
+            }
+        } while (answer != number);
     }
 }
