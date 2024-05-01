6e3445e59c11b19f57be91f99306925f74bf4462
Bernardo Marques <bernardohmarques@gmail.com>
2024-05-01 05:25:52 -0300

    lesson

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software engineer";
+        job1._company = "Controlle";
+        job1._startYear = 2022;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Junior developer";
+        job2._company = "Weex";
+        job2._startYear = 2020;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Bernardo Marques";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
